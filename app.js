5bcd07e800fe57d5724a137a4d3e243a69429a38
Loren Meta <loren_meta@yahoo.com>
2022-07-15 00:17:47 +0200

    adding style

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ addItemBtn.addEventListener('click', () => {
     let inputValue = input.value;
     if (inputValue !== '') {
         let li = document.createElement('li');
-        let btn = document.createElement('button');
-        btn.className = "liBtn";
-        btn.textContent = "Remove"
-        li.innerHTML = inputValue;
-        li.appendChild(btn);
+        let input = document.createElement('input');
+        input.className = "li-input";
+        input.value = inputValue;
+        let removeBtn = document.createElement('button');
+        removeBtn.className = "removeBtn";
+        removeBtn.textContent = "Remove";
+        li.appendChild(input);
+        li.appendChild(removeBtn);
         ulList.appendChild(li);
+        p.innerHTML = '';
+
+        removeBtn = document.querySelector('.removeBtn');
+        removeBtn.addEventListener('click', () => {
+            ulList.removeChild(li);
+        })
     } else {
         p.innerHTML = 'Please type something...'
     }
+});
 
-})
